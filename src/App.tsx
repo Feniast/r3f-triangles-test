9ae22e80e90813705d528a840e6516ccfbddd8c6
Feniast <faunreal@hotmail.com>
2020-09-29 17:26:30 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
   extend,
   useThree,
   useUpdate,
+  createPortal,
 } from "react-three-fiber";
 import * as THREE from "three";
 import { OrbitControls, shaderMaterial, useTextureLoader, Html } from "drei";
@@ -28,8 +29,10 @@ import { PlaneBufferGeometry } from "three";
 import tri from "url:./assets/tri.png";
 import mask1Image from "url:./assets/mask1.jpg";
 import mask2Image from "url:./assets/mask2.jpg";
+import mask3Image from "url:./assets/mask3.jpg";
 import image1 from "url:./assets/image1.jpg";
 import image2 from "url:./assets/image2.jpg";
+import image3 from "url:./assets/image3.jpg";
 import useSWR from "swr";
 
 interface DatGuiSetting {
@@ -43,6 +46,7 @@ interface DatGuiSetting {
 const ImgShaderMaterial = shaderMaterial(
   {
     image: null,
+    fg: null
   },
   vertex,
   fragment,
@@ -64,6 +68,15 @@ extend({
   ParticlesShaderMaterial,
   ImgShaderMaterial,
 });
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      particlesShaderMaterial: any;
+      imgShaderMaterial: any;
+    }
+  }
+}
 
 const useDatGui = <T extends Record<string, DatGuiSetting>>(settings: T) => {
   const obj = useDeepMemo<Record<keyof T, DatGuiSetting["value"]>>(() => {
@@ -168,20 +181,35 @@ interface PointsProps {
   speedScale?: number;
   maskImage: string;
   image: string;
+  sampleStep?: {
+    x?: number;
+    y?: number;
+  };
 }
 
 const Points: React.FC<PointsProps> = (props) => {
-  let { imageScale = 100 / 1920, speedScale = 0.0006, maskImage, image } = props;
-  const { clock, size } = useThree();
+  let {
+    imageScale = 100 / 1920,
+    speedScale = 0.0006,
+    maskImage,
+    image,
+    sampleStep = { x: 1, y: 1 },
+  } = props;
+  const { clock, size, camera } = useThree();
   const { data: maskData } = useImageData({
     image: maskImage,
     scale: imageScale,
   });
 
+  let { x: xStep = 1, y: yStep = 1 } = sampleStep;
+  xStep = Math.max(xStep, 1);
+  yStep = Math.max(yStep, 1);
+
   const imageTexture = useTextureLoader(image);
 
   const positionScale = 1 / maskData.height;
 
+  const aspect = size.width / size.height;
   const imageAspect = maskData.width / maskData.height;
 
   const triTexture = useTextureLoader(tri);
@@ -195,15 +223,15 @@ const Points: React.FC<PointsProps> = (props) => {
     const rot: number[] = [];
     speeds.current = [];
     const { width, height, data } = maskData;
-    for (let i = 0; i < width; i++) {
-      for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i += xStep) {
+      for (let j = 0; j < height; j += yStep) {
         const p = data[(j * width + i) * 4];
         if (p === 255) {
           const x = (i - width * 0.5) * positionScale;
           const y = -(j - height * 0.5) * positionScale;
           const z = Math.random() * 0.5 + 0.5;
           vertices.push(x, y, z);
-          alphas.push(Math.random() * 0.8 + 0.1);
+          alphas.push(Math.random() * 0.6 + 0.4);
           colors.push(random(0, Colors.length, true));
           rot.push(Math.random() * Math.PI * 2);
           speeds.current.push(Math.random() * speedScale * 0.5 + speedScale);
@@ -228,6 +256,7 @@ const Points: React.FC<PointsProps> = (props) => {
 
   const particlesMaterial = useRef<THREE.ShaderMaterial>();
   const imgMaterial = useRef<THREE.ShaderMaterial>();
+  const imgMaterial2 = useRef<THREE.ShaderMaterial>();
 
   const update = () => {
     const position = geometry.attributes.position;
@@ -264,13 +293,9 @@ const Points: React.FC<PointsProps> = (props) => {
     rot.needsUpdate = true;
   };
 
-  useFrame(() => {
-    particlesMaterial.current.uniforms.time.value = clock.elapsedTime;
-    update();
-  });
-
   const pointsMesh = useRef<THREE.Points>();
   const imageMesh = useRef<THREE.Mesh>();
+  const imageMesh2 = useRef<THREE.Mesh>();
   useLayoutEffect(() => {
     const aspect = size.width / size.height;
     let s = 1;
@@ -283,14 +308,46 @@ const Points: React.FC<PointsProps> = (props) => {
     pointsMesh.current.position.y = y;
     pointsMesh.current.scale.set(s, s, s);
 
-    imageMesh.current.scale.set(s * imageAspect, s, s);
-    imageMesh.current.position.y = y;
+    // imageMesh.current.scale.set(s, s, s);
+    // imageMesh.current.position.y = y;
+    // imageMesh2.current.scale.set(s, s, s);
+    // imageMesh2.current.position.y = y;
   }, [size.width, size.height]);
 
-  return (
-    <group>
-      <points position-z={0} ref={pointsMesh} geometry={geometry}>
-        {/* @ts-ignore */}
+  const renderTarget = useMemo(
+    () =>
+      new THREE.WebGLRenderTarget(size.width, size.height, {
+        encoding: THREE.sRGBEncoding,
+        generateMipmaps: true,
+        minFilter: THREE.LinearMipmapLinearFilter,
+      }),
+    []
+  );
+  const offScene = useMemo(() => new THREE.Scene(), []);
+  const offCamera = useMemo(() => {
+    const c = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1000);
+    c.position.set(0, 0, 2);
+    c.lookAt(0, 0, 0);
+    return c;
+  }, []);
+  useLayoutEffect(() => {
+    const aspect = size.width / size.height;
+    offCamera.left = -1 * 0.5 * aspect;
+    offCamera.right = 1 * 0.5 * aspect;
+    offCamera.top = 1 * 0.5;
+    offCamera.bottom = -1 * 0.5;
+    offCamera.updateProjectionMatrix();
+  }, [offCamera, size.width, size.height]);
+
+  const offSceneContent = createPortal(
+    <>
+      {/* <primitive object={offCamera} /> */}
+      <points
+        position-z={0.002}
+        ref={pointsMesh}
+        geometry={geometry}
+        visible={true}
+      >
         <particlesShaderMaterial
           ref={particlesMaterial}
           depthWrite={false}
@@ -301,31 +358,74 @@ const Points: React.FC<PointsProps> = (props) => {
           colors={Colors}
         />
       </points>
-      <mesh ref={imageMesh}>
-        <planeBufferGeometry args={[1, 1]} attach="geometry" />
-        {/* @ts-ignore */}
-        <imgShaderMaterial
-          ref={imgMaterial}
-          transparent
-          attach="material"
-          image={imageTexture}
-          blending={THREE.AdditiveBlending}
-        />
-      </mesh>
-    </group>
+    </>,
+    offScene
+  );
+
+  useFrame(({ gl, scene, camera }) => {
+    particlesMaterial.current.uniforms.time.value = clock.elapsedTime;
+    update();
+    renderTarget.width = size.width;
+    renderTarget.height = size.height;
+    gl.setRenderTarget(renderTarget);
+    gl.render(offScene, offCamera);
+    gl.setRenderTarget(null);
+    imgMaterial2.current.needsUpdate = true;
+  });
+
+  return (
+    <>
+      {offSceneContent}
+      <group>
+        {/* <points position-z={0} ref={pointsMesh} geometry={geometry}>
+          <particlesShaderMaterial
+            ref={particlesMaterial}
+            depthWrite={false}
+            depthTest={false}
+            transparent
+            attach="material"
+            tex={triTexture}
+            colors={Colors}
+          />
+        </points> */}
+        {/* <mesh ref={imageMesh} visible={true}>
+          <planeBufferGeometry args={[imageAspect, 1]} attach="geometry" />
+          <imgShaderMaterial
+            ref={imgMaterial}
+            transparent
+            attach="material"
+            image={imageTexture}
+          />
+        </mesh> */}
+        <mesh position-z={0.001} ref={imageMesh2} visible={true}>
+          <planeBufferGeometry args={[aspect, 1]} attach="geometry" />
+          <imgShaderMaterial
+            transparent
+            attach="material"
+            image={imageTexture}
+            fg={renderTarget.texture}
+            ref={imgMaterial2}
+          />
+        </mesh>
+      </group>
+    </>
   );
 };
 
 const Scene = () => {
   return (
     <>
-      <Points maskImage={mask1Image} image={image1} />
+      <Points
+        maskImage={mask3Image}
+        image={image3}
+        sampleStep={{ x: 1.5, y: 2 }}
+      />
     </>
   );
 };
 
 const CameraSet = () => {
-  const { size, setDefaultCamera, camera: dCamera } = useThree();
+  const { size, setDefaultCamera } = useThree();
   const frustumSize = 1;
   const camera = useMemo(() => {
     const c = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1000);
@@ -353,7 +453,7 @@ const App = () => {
         colorManagement
         updateDefaultCamera={false} // not update our custom camera
         onCreated={(ctx) => {
-          ctx.gl.setClearColor(0x000000);
+          // ctx.gl.setClearColor(0x000000);
         }}
       >
         <CameraSet />
--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -1,5 +1,15 @@
 varying vec2 vUv;
 uniform sampler2D image;
+uniform sampler2D fg;
+
+float overlay(float x, float y) {
+	return (x < 0.5) ? (2.0 * x * y) : (1.0 - 2.0 * (1.0 - x) * (1.0 - y));
+}
+
+vec4 overlayBlending(vec4 x, vec4 y, float opacity) {
+	vec4 z = vec4(overlay(x.r, y.r), overlay(x.g, y.g), overlay(x.b, y.b), overlay(x.a, y.a));
+	return z * opacity + x * (1.0 - opacity);
+}
 
 void main() {
   gl_FragColor = texture2D(image, vUv);
